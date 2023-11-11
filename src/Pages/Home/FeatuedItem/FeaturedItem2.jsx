3ebd0700e1af29f1132f9a02d21e41ba10bd0da5
wolfiee42 <saifalislam2022@gmail.com>
2023-11-11 21:54:38 +0600

    second feature section added

---
+++ src/Pages/Home/FeatuedItem/FeaturedItem2.jsx
@@ -0,0 +1,19 @@
+import "./featured.css"
+
+
+const FeaturedItem2 = () => {
+    return (
+        <div className="max-w-7xl mx-auto my-36">
+            <div className="featuredItem2 min-h-[500px] bg-top ">
+                <div className="absolute">
+                    <div className="bg-white flex flex-col justify-center items-center  h-[250px] mx-auto w-[700px] relative top-32 left-[300px]">
+                        <h1 className="text-4xl mb-5">Bistro Boss</h1>
+                        <p className="text-base max-w-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. At est eaque, molestias dolores provident consequuntur iusto sequi asperiores architecto, rem illo natus vitae. Magni ipsa architecto perferendis vero commodi alias!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedItem2;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import FeaturedItem from "../FeatuedItem/FeaturedItem";
+import FeaturedItem2 from "../FeatuedItem/FeaturedItem2";
 import PopularMenu from "../Popular menu/PopularMenu";
 import Review from "../Review/Review";
 
@@ -9,6 +10,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Category></Category>
+            <FeaturedItem2></FeaturedItem2>
             <PopularMenu></PopularMenu>
             <FeaturedItem></FeaturedItem>
             <Review></Review>
